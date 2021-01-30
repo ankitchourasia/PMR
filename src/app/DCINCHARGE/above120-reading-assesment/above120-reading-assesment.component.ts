87f9651327cb84d08a036b9066e0adde7a511c8d
unknown <ankit.chourasia@outlook.com>
2021-01-30 17:19:15 +0530

    reading verification changes - wip

--- src/app/DCINCHARGE/above120-reading-assesment/above120-reading-assesment.component.ts
+++ src/app/DCINCHARGE/above120-reading-assesment/above120-reading-assesment.component.ts
@@ -37,11 +37,12 @@ globalCounter:number=0;
 
   ngOnInit() 
   {
-    if(this.session.get('check')=="FOUNDOK" && this.session.get('role')=="DCINCHARGE")
-    {
+    if(this.session.get('check')=="FOUNDOK" && this.session.get('role')=="DCINCHARGE"){
       this.groupno=this.activeroute.snapshot.params.groupno;
       this.billmonth=this.activeroute.snapshot.params.billmonth;
-      this.getGroupReadingAbove120();
+      // this.getGroupReadingAbove120();
+      this.activeTab = "ABOVE_120";
+      this.getReading();
       this.SelectedList=Array<any>();
       this.SelectedImageId=Array<any>();
     }
@@ -65,69 +66,225 @@ globalCounter:number=0;
        });
     }
   
-    getGroupReadingAbove120()
-    {
+    // getGroupReadingAbove120()
+    // {
      
-      this.loading=true;
-      let formdata : FormData = new FormData();
-      formdata.append("billmonth",this.enc.encrypt(this.billmonth));
-      formdata.append("groupno",this.enc.encrypt(this.groupno));
-      formdata.append("loccode",this.enc.encrypt(this.loccode));
+    //   this.loading=true;
+    //   let formdata : FormData = new FormData();
+    //   formdata.append("billmonth",this.enc.encrypt(this.billmonth));
+    //   formdata.append("groupno",this.enc.encrypt(this.groupno));
+    //   formdata.append("loccode",this.enc.encrypt(this.loccode));
       
-      return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-above-120", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>
-      {
-        this.resp=response;
-        if(this.resp.length>0)
-        {
-          for(let data of this.resp)
-      {
-        let date = data.datereading;
-        //alert(date);
-        let n=date.indexOf(' ');
-        this.dateofreading=date.substr(0,n);        
-      }
-          this.showdata=false;
-           this.loading=false;
-        }
-        else
-        {
-          this.showdata=true;
-          this.loading=false;
+    //   return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-above-120", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>
+    //   {
+    //     this.resp=response;
+    //     if(this.resp.length>0)
+    //     {
+    //       for(let data of this.resp)
+    //   {
+    //     let date = data.datereading;
+    //     //alert(date);
+    //     let n=date.indexOf(' ');
+    //     this.dateofreading=date.substr(0,n);        
+    //   }
+    //       this.showdata=false;
+    //        this.loading=false;
+    //     }
+    //     else
+    //     {
+    //       this.showdata=true;
+    //       this.loading=false;
          
-        }
-      },
-      error=>{
-      this.error = error
-      this.loading=false;
+    //     }
+    //   },
+    //   error=>{
+    //   this.error = error
+    //   this.loading=false;
   
-      alert('Server not responding');
-      });
-    }
+    //   alert('Server not responding');
+    //   });
+    // }
   
-    getGroupReadingsDatewise()
-    {
+    activeTab : String;
+    getReadingsDatewise(){
+      this.activeTab = "ALL";
       this.router.navigate(['/reading-assesment-group-wise',this.groupno,this.billmonth])
     }
   
-    getGroupReadingBelow90()
-    {
-      this.router.navigate(['/below90-reading-assesment',this.groupno,this.billmonth])
+    getReadingBelow90(){
+      this.activeTab = "BELOW_90";
+      // this.router.navigate(['/below90-reading-assesment',this.groupno,this.billmonth])
+      this.getReading();
     }
   
-    getGroupReadingbetween90to120()
-    {
-      this.router.navigate(['/assesment90-to120-reading',this.groupno,this.billmonth])
+    getReadingbetween90to120(){
+      this.activeTab = "BETWEEN_90_AND_120";
+      // this.router.navigate(['/assesment90-to120-reading',this.groupno,this.billmonth])
+      this.getReading();
     }
   
-    getGroupReadingMeterDefective()
-    {
-      this.router.navigate(['/meterdefective-reading-assesment',this.groupno,this.billmonth])
+    getReadingMeterDefective(){
+      this.activeTab = "METER_DEFCTIVE";
+      // this.router.navigate(['/meterdefective-reading-assesment',this.groupno,this.billmonth])
+      this.getReading();
     }
   
-    getGroupReadingMeterNotFound()
-    {
-      this.router.navigate(['/meternotfound-reading-assesment',this.groupno,this.billmonth])
+    getReadingMeterNotFound(){
+      this.activeTab = "METER_NOT_FOUND";
+      // this.router.navigate(['/meternotfound-reading-assesment',this.groupno,this.billmonth])
+      this.getReading();
     }
+
+    getReadingAbove120(){
+      this.activeTab = "ABOVE_120";
+      this.getReading();
+    }
+
+    getReading(){
+      let formData : FormData = new FormData();
+      formData.append("billmonth",this.enc.encrypt(this.billmonth));
+      formData.append("groupno",this.enc.encrypt(this.groupno));
+      formData.append("loccode",this.enc.encrypt(this.loccode));
+      if(this.activeTab === "ABOVE_120"){
+        this.getReadingsAbove120(formData);
+      } else if(this.activeTab === "BELOW_90"){
+        this.getReadingsBelow90(formData);
+      } else if(this.activeTab === "BETWEEN_90_AND_120"){
+        this.getReadingsBetween90And120(formData);
+      } else if(this.activeTab === "METER_DEFCTIVE"){
+        this.getReadingsMeterDefective(formData);
+      } else if(this.activeTab === "METER_NOT_FOUND"){
+        this.getReadingsMeterNotFound(formData);
+      } else if(this.activeTab === "ALL"){
+        this.getReadingsDatewise();
+      }
+    }
+
+    getReadingsAbove120(formData){
+      this.loading = true;
+      return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-above-120", formData, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>{
+          this.resp=response;
+        if(this.resp.length>0){
+          for(let data of this.resp){
+            let date = data.datereading;
+            //alert(date);
+            let n=date.indexOf(' ');
+            this.dateofreading=date.substr(0,n);        
+          }
+            this.showdata=false;
+             this.loading=false;
+        }
+        else{
+          this.showdata=true;
+          this.loading=false;
+        }
+      },error=>{
+        this.error = error
+        this.loading=false;
+        alert('Server not responding');
+        });
+    }
+
+    getReadingsBelow90(formData){
+      this.loading = true;
+      return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-below-then-90", formData, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>{
+          this.resp=response;
+        if(this.resp.length>0){
+          for(let data of this.resp){
+            let date = data.datereading;
+            //alert(date);
+            let n=date.indexOf(' ');
+            this.dateofreading=date.substr(0,n);        
+          }
+            this.showdata=false;
+             this.loading=false;
+        }
+        else{
+          this.showdata=true;
+          this.loading=false;
+        }
+      },error=>{
+        this.error = error
+        this.loading=false;
+        alert('Server not responding');
+        });
+    }
+
+    getReadingsBetween90And120(formData){
+      this.loading = true;
+      return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-in-90-to-120", formData, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>{
+          this.resp=response;
+        if(this.resp.length>0){
+          for(let data of this.resp){
+            let date = data.datereading;
+            //alert(date);
+            let n=date.indexOf(' ');
+            this.dateofreading=date.substr(0,n);        
+          }
+            this.showdata=false;
+             this.loading=false;
+        }
+        else{
+          this.showdata=true;
+          this.loading=false;
+        }
+      },error=>{
+        this.error = error
+        this.loading=false;
+        alert('Server not responding');
+        });
+    }
+
+    getReadingsMeterDefective(formData){
+      this.loading = true;
+      return this.http.post("api/assesment-records/get-record-group-wise-for-assesment-meter-defective", formData, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>{
+          this.resp=response;
+        if(this.resp.length>0){
+          for(let data of this.resp){
+            let date = data.datereading;
+            //alert(date);
+            let n=date.indexOf(' ');
+            this.dateofreading=date.substr(0,n);        
+          }
+            this.showdata=false;
+             this.loading=false;
+        }
+        else{
+          this.showdata=true;
+          this.loading=false;
+        }
+      },error=>{
+        this.error = error
+        this.loading=false;
+        alert('Server not responding');
+        });
+    }
+
+    getReadingsMeterNotFound(formData){
+      this.loading = true;
+      return this.http.post("api/assesment-records/get-record-for-group-wise-for-assesment-meter-notfound", formData, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>{
+          this.resp=response;
+        if(this.resp.length>0){
+          for(let data of this.resp){
+            let date = data.datereading;
+            //alert(date);
+            let n=date.indexOf(' ');
+            this.dateofreading=date.substr(0,n);        
+          }
+            this.showdata=false;
+             this.loading=false;
+        }
+        else{
+          this.showdata=true;
+          this.loading=false;
+        }
+      },error=>{
+        this.error = error
+        this.loading=false;
+        alert('Server not responding');
+        });
+    }
+
 VerifyClick(custid,imageid,srno)
 {
 
@@ -210,7 +367,8 @@ verifyReading(custid,imageid)
     {
     
     alert(this.resp1.msg);
-    this.getGroupReadingAbove120();
+    $("#myModal11").modal("hide");
+    this.getReading();
       
   }
     else
@@ -224,15 +382,61 @@ verifyReading(custid,imageid)
   });
 }
 
-
-OnclickGetvalue(imageid,custid,billmonth,assesment){
-
-  this.custid=custid;
-  this.imageid=imageid;
-  this.assesment=assesment;
-  this.bilmonthfor=billmonth;
+index : any;
+OnclickGetvalue(data){
+  this.index = this.resp.indexOf(data);
+  this.getImage1(data.imagepath);
+  this.getImage2(data.image2path);
+  this.custid=data.custid;
+  this.imageid=data.imageid;
+  this.assesment=data.assesment;
+  this.bilmonthfor=data.billmonth;
  
 // console.log(imageid+""+custid+""+billmonth+""+assesment);
+}
+
+image1 : any;
+getImage1(imagePath){
+  console.log("inside get image");
+  this.http.get("api/mtr-reader/get-meter-reader-image?path=" + imagePath, { responseType: 'blob' }).subscribe(success =>{
+    console.log(success);
+    this.createImage1FromBlob(success);
+  }, error =>{
+    console.log(error);
+  })
+}
+
+image2: any;
+getImage2(imagePath){
+  console.log("inside get image");
+  this.http.get("api/mtr-reader/get-meter-reader-image?path=" + imagePath, { responseType: 'blob' }).subscribe(success =>{
+    console.log(success);
+    this.createImage2FromBlob(success);
+  }, error =>{
+    console.log(error);
+  })
+}
+
+createImage1FromBlob(image: Blob) {
+   let reader = new FileReader();
+   reader.addEventListener("load", () => {
+      this.image1 = reader.result;
+   }, false);
+
+   if (image) {
+      reader.readAsDataURL(image);
+   }
+}
+
+createImage2FromBlob(image: Blob) {
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+     this.image2 = reader.result;
+  }, false);
+
+  if (image) {
+     reader.readAsDataURL(image);
+  }
 }
 
 verifySelectedReading()
@@ -267,7 +471,7 @@ verifySelectedReading()
         alert(this.resp2.msg);
       //     this.SelectedList=Array<any>();
       // this.SelectedImageId=Array<any>();
-        this.getGroupReadingAbove120();
+        this.getReading();
       }
     }
     
@@ -310,16 +514,54 @@ editAssesment()
     });
   }
 
-  deleteReading(custid)
+  // deleteReading(custid)
+  // {
+  //   $("#myModal11").modal("hide");
+  //   this.session.set("custid",custid);
+  //   this.session.set("billmonth",this.billmonth);
+  //   this.navCtrl.navigate('delete-reading-remark',{custid:custid,billmonth:this.billmonth});
+  // }
+
+  deleteReading()
   {
-    this.session.set("custid",custid);
-    this.session.set("billmonth",this.billmonth);
-    this.navCtrl.navigate('delete-reading-remark',{custid:custid,billmonth:this.billmonth});
+   
+    let formdata : FormData = new FormData();
+    formdata.append("custid",this.enc.encrypt(this.custid));
+    formdata.append("billmonth",this.enc.encrypt(this.billmonth));
+    formdata.append("userid",this.enc.encrypt(this.loccode));
+    formdata.append("loccode",this.enc.encrypt(this.loccode));
+    formdata.append("remark",this.enc.encrypt("Deleted by DC"));
+    
+    return this.http.post("api/reading-operations/delete-a-reading", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(response=>
+    {
+      let successResponse : any = <any> response;
+      if(successResponse){
+        alert(successResponse.msg);
+        // this.router.navigateByUrl('/dcinch_dashboard', {skipLocationChange: true}).then(()=>
+        // this.router.navigate(["/reading-for-verification"])); 
+        // this.router.navigate(['/']);
+        this.resp.splice(this.index, 1);
+        this.OnclickGetvalue(this.resp[this.index]);
+      }
+    },
+    error=>{
+      this.error = error
+      alert('Server not responding');
+      });
   }
 
   exportAsXsls(): void {
     this.exportService.exportAsExcelFile(this.resp, 'above 120'+this.groupno+"_"+this.billmonth);
   
   }
+
+  nextClicked(){
+    let data1 = this.resp[this.index + 1];
+    this.OnclickGetvalue(data1);
+  }
   
+  previousButtonClicked(){
+    let data1 = this.resp[this.index - 1];
+    this.OnclickGetvalue(data1);
+  }
 }
