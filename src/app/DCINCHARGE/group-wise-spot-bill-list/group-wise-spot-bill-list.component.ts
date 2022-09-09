9da4b2897ba4710a27d93e3dbb9317c05ab67d5e
unknown <ankit.chourasia@outlook.com>
2022-09-09 13:03:01 +0530

    changes in group wise spot bill report

--- src/app/DCINCHARGE/group-wise-spot-bill-list/group-wise-spot-bill-list.component.ts
+++ src/app/DCINCHARGE/group-wise-spot-bill-list/group-wise-spot-bill-list.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { SessionStorageService } from 'angular-web-storage';
 import { EncreptiondecreptionService } from 'src/app/Util-services/encreptiondecreption.service';
+import { ExportExcelService } from 'src/app/Util-services/export-excel.service';
 import { GlobalConstants } from 'src/app/Util-services/global-constants';
 
 @Component({
@@ -18,8 +19,9 @@ export class GroupWiseSpotBillListComponent implements OnInit {
   groups : any = [];
   spotBillList : any = [];
   groupno;
+  status = 'ALL';
   constructor(private session : SessionStorageService, private http: HttpClient, 
-    private enc: EncreptiondecreptionService) { }
+    private enc: EncreptiondecreptionService, private exportService : ExportExcelService) { }
 
   ngOnInit() {
     this.getGroupsByLocationCode();
@@ -39,20 +41,65 @@ export class GroupWiseSpotBillListComponent implements OnInit {
     });
   }
 
+  rdNumbers : any = [];
+  rdNo : string;
+  getRDByGroupNo(){
+    let formdata : FormData = new FormData();
+    formdata.append("loccode",this.enc.encrypt(this.locationCode));
+    formdata.append("grno",this.enc.encrypt(this.groupno));
+    return this.http.post("api/master-rec/get-list-of-rds-in-groups-in-dc", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(success=>{
+      console.log(success);
+      this.rdNumbers = success;
+    },
+    error=>{
+      console.log(error);
+    });
+  }
+
   getGroupWiseSpotBill(){
     this.spotBillList = [];
+    this.listToShow = [];
+    this.successList = [];
+    this.errorList = [];
     this.loading = true;
+    this.status = 'ALL';
     let formdata : FormData = new FormData();
     formdata.append("loccode",this.enc.encrypt(this.locationCode));
     formdata.append("billmonth",this.enc.encrypt(this.billmon));
     formdata.append("groupno",this.enc.encrypt(this.groupno));
+    formdata.append("rdno",this.enc.encrypt(this.rdNo));
     return this.http.post("api/spot-bill-reporter/get-list-for-group", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(success=>{
       this.loading = false;
       this.spotBillList = success;
+      this.listToShow = Object.assign([], this.spotBillList);
+      this.separateByStatus();
     }, error=>{
       this.loading = false;
       console.log(error);
     });
+  }
+
+  listToShow = [];
+  successList = [];
+  errorList = [];
+  separateByStatus(){
+    this.spotBillList.forEach(element => {
+      if(element.ngbuploadstatus === "UPLOADEDSUCCESSFULLY"){ 
+        this.successList.push(element);
+      } else if(element.ngbuploadstatus === "ERRORINUPLOAD"){
+        this.errorList.push(element);
+      }
+    });
+  }
+
+  statusChanged(){
+    if(this.status === "SUCCESS"){
+      this.listToShow = Object.assign([], this.successList);
+    } else if(this.status === "ERROR"){
+      this.listToShow = Object.assign([], this.errorList);
+    } else if(this.status === "ALL"){
+      this.listToShow = Object.assign([], this.spotBillList);
+    }
   }
 
   repushREes
@@ -66,9 +113,8 @@ export class GroupWiseSpotBillListComponent implements OnInit {
     return this.http.post("api/spotbill-uploader/re-push-spot-bill", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(success=>{
       this.loading = false;
       this.repushREes =success
-        alert(this.repushREes.msg)
-      
-      // this.spotBillList = success;
+      alert(this.repushREes.msg)
+      this.getGroupWiseSpotBill();
     }, error=>{
       this.loading = false;
       console.log(error);
@@ -76,4 +122,29 @@ export class GroupWiseSpotBillListComponent implements OnInit {
     });
   }
 
+  deleteReading(data){
+    if(confirm("Are you sure, you want to delete Read ?")){
+      if(data.ngbuploadstatus !== "ERRORINUPLOAD"){
+        return;
+      }
+      let formdata = new FormData();
+      formdata.append("consno",this.enc.encrypt(data.consumerno));
+      formdata.append("billmon",this.enc.encrypt(this.billmon));
+  
+      return this.http.post("api/spot-bill-reporter/delete-spot-bill", formdata, {headers:new HttpHeaders().set('Authorization',this.session.get('token'))}).subscribe(success=>{
+        this.loading = false;
+        alert("Deleted Successfully");
+        this.getGroupWiseSpotBill();
+      }, error=>{
+        this.loading = false;
+        console.log(error);
+        alert("Unable to delete");
+      });
+    }
+  }
+
+  exportButtonClicked(){
+    this.exportService.exportAsExcelFile(this.spotBillList, "SPOT_BILL_" + this.groupno + "_" + this.rdNo);
+  }
+  
 }
